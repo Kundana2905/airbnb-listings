ac445a2cb4e039a88f8121cb4554d9c5ef3d731f
Kundana Thutakura <kundanathutakura@gmail.com>
2025-07-23 14:03:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask ,render_template,request
+import joblib
+import pandas as pd
+
+app=Flask(__name__)
+
+model = joblib.load("D:\\project\\airbnb.pkl")
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/predict", methods=['POST'])
+def home():
+    if request.method=="POST":
+        data={
+            "City":request.form.get("City"),
+            "RoomType":request.form.get("RoomType"),
+            "Bedrooms":int(request.form.get("Bedrooms")),
+            "Bathrooms":int(request.form.get("Bathrooms")),
+            "GuestsCapacity":int(request.form.get("GuestsCapacity")),
+            "HasWifi":int(request.form.get("HasWifi")),
+            "HasAC":int(request.form.get("HasAC")),
+            "DistanceFromCityCentre":request.form.get("DistanceFromCityCentre")
+        }
+        df=pd.DataFrame([data])
+        prediction=model.predict(df)[0]
+        return render_template("index.html",predictt=prediction)
+    else:
+        return render_template("index.html")
+if __name__=="__main__":
+    app.run()
